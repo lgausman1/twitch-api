92d9e32a07724d497a6fb6a56084469bbbfa760f
lgausman1 <l.gausman@comcast.net>
2015-11-08 15:41:28 -0800

    added search button and feeds limit & offset to query string

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 // dynamically build script tag and insert in header
 var streamSrc = 'starcraft';
+var offset = 0;
 var head = document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = 'https://api.twitch.tv/kraken/search/streams?q=' + streamSrc + '&callback=myCallback';
+script.src = 'https://api.twitch.tv/kraken/search/streams?q=' + streamSrc + '&callback=myCallback' + '&limit=10&offset=' + offset + '';
 head.appendChild(script);
 
 // retrieve data from api
 function myCallback(data){
 
   	console.log(data);
-	// declare variables for feeds
+	// declare variables and build feeds
 	var feedContainer = document.getElementById("feeds");
 	var feedDiv = document.createDocumentFragment();
+	var feedTotal = document.createElement('p');
+	feedTotal.textContent = "Total results: " + data._total;
 	var feedList = document.createElement('ul');
 	var streamsLen = data.streams.length;
+
 	for (var i = 0; i < streamsLen; i++) {
-		// console.log(data.streams[i].channel.name);
-		// console.log(data.streams[i].game);
+
 		var li = document.createElement('li');
 		var streamTitle = document.createElement('h2');
 		var img = new Image();
@@ -37,10 +40,15 @@ function myCallback(data){
 		gameTitle.appendChild(viewerSpan);
 		li.appendChild(gameDesc);
 		feedList.appendChild(li);
+		feedDiv.appendChild(feedTotal);
 		feedDiv.appendChild(feedList); // this is my doc frag
 		feedContainer.appendChild(feedDiv);
 
-	}
+	} // end for loop
+
+	// build next and previous links
+	// data._links.next: "https://api.twitch.tv/kraken/search/streams?limit=10&offset=10&q=starcraft"
+	// data._links.self: "https://api.twitch.tv/kraken/search/streams?limit=10&offset=0&q=starcraft"
     
 }
 
