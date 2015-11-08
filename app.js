104bc7f034d58e0af9b94a78fd9571c74fd7847e
lgausman1 <l.gausman@comcast.net>
2015-11-08 10:16:11 -0800

    loading stream name and photo in DOM

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// 
+// dynamically build script tag and insert in header
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://api.twitch.tv/kraken/search/streams?q=starcraft&callback=myCallback';
+head.appendChild(script);
+
+// retrieve data from api
+function myCallback(data){
+
+  	console.log(data);
+  	// data.streams[i].preview.small;
+	// declare variables for feeds
+	var feedContainer = document.getElementById("feeds");
+	var feedDiv = document.createDocumentFragment();
+	var feedList = document.createElement('ul');
+	var streamsLen = data.streams.length;
+	for (var i = 0; i < streamsLen; i++) {
+		console.log(data.streams[i].channel.name);
+		console.log(data.streams[i].preview.small);
+		var li = document.createElement('li');
+		var img = new Image();
+		img.src = data.streams[i].preview.small;
+
+		li.textContent = data.streams[i].channel.name;
+		li.appendChild(img);
+		feedList.appendChild(li);
+		feedDiv.appendChild(feedList);
+		feedContainer.appendChild(feedDiv);
+
+	}
+    //var twitterEntry;
+    //var text = '';
+    //var len = dataWeGotViaJsonp.length;
+    //for(var i=0;i<len;i++){
+    //    twitterEntry = dataWeGotViaJsonp[i];
+    //    //text += '<p>' + twitterEntry['text'] + '</p>'
+    //    console.log(twitterEntry);
+    //}
+    //document.getElementById('feeds').innerHTML = data.streams[0].channel.name;
+    // return data;
+    
+}
+
+
+
+
