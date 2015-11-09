5de8419107f277dc1309a26fdd0acecfcce1ba08
lgausman1 <l.gausman@comcast.net>
2015-11-09 08:06:02 -0800

    query results rendering on DOM. refactored script loading and DOM loading

--- app.js
+++ app.js
@@ -6,24 +6,22 @@ var searchBtn = document.getElementById('searchBtn');
 var searchInput = document.getElementById('streamSearch');
 
 searchBtn.addEventListener('click', searchStreams, false);
-// element.addEventListener('click', myFunctionReference , false);
 function searchStreams() {
-	// if searchInput value is null set default search
+	// if searchInput value is null 
 	if(searchInput.value.length == 0) {
 		alert("enter a stream to search");
 	}
 	// else streamSrc equals input value
 	else {
 		streamSrc = searchInput.value;
-		//alert(streamSrc);
 		buildQuery();
+
 	}
 }
 
-
 // dynamically build script tag and insert in header
 function buildQuery() {
-	var head = document.getElementsByTagName('head')[0];
+	var head = document.head;
 	var script = document.createElement('script');
 	script.type = 'text/javascript';
 	script.src = 'https://api.twitch.tv/kraken/search/streams?q=' + streamSrc + '&callback=myCallback' + '&limit=10&offset=' + offset + '';
@@ -42,36 +40,39 @@ function myCallback(data){
 	var feedList = document.createElement('ul');
 	var streamsLen = data.streams.length;
 
-	for (var i = 0; i < streamsLen; i++) {
+	function buildFeed() {
+		// clear out previous feed if any
+		feedContainer.innerHTML = "";
 
-		var li = document.createElement('li');
-		var streamTitle = document.createElement('h2');
-		var img = new Image();
-		var gameTitle = document.createElement('p');
-		var viewerSpan = document.createElement('span');
-		var gameDesc = document.createElement('p');
+		for (var i = 0; i < streamsLen; i++) {
 
-		img.src = data.streams[i].preview.medium;
-		streamTitle.textContent = data.streams[i].channel.display_name;
-		gameTitle.textContent = data.streams[i].game;
-		viewerSpan.textContent = " - " + data.streams[i].viewers + " viewers";
-		viewerSpan.className = 'viewers';
-		gameDesc.textContent = data.streams[i].channel.status;
-		li.appendChild(img);
-		li.appendChild(streamTitle);
-		li.appendChild(gameTitle);
-		gameTitle.appendChild(viewerSpan);
-		li.appendChild(gameDesc);
-		feedList.appendChild(li);
-		feedDiv.appendChild(feedTotal);
-		feedDiv.appendChild(feedList); // this is my doc frag
-		feedContainer.appendChild(feedDiv);
+			var li = document.createElement('li');
+			var streamTitle = document.createElement('h2');
+			var img = new Image();
+			var gameTitle = document.createElement('p');
+			var viewerSpan = document.createElement('span');
+			var gameDesc = document.createElement('p');
 
-	} // end for loop
+			img.src = data.streams[i].preview.medium;
+			streamTitle.textContent = data.streams[i].channel.display_name;
+			gameTitle.textContent = data.streams[i].game;
+			viewerSpan.textContent = " - " + data.streams[i].viewers + " viewers";
+			viewerSpan.className = 'viewers';
+			gameDesc.textContent = data.streams[i].channel.status;
+			li.appendChild(img);
+			li.appendChild(streamTitle);
+			li.appendChild(gameTitle);
+			gameTitle.appendChild(viewerSpan);
+			li.appendChild(gameDesc);
+			feedList.appendChild(li);
+			feedDiv.appendChild(feedTotal);
+			feedDiv.appendChild(feedList); // this is my doc frag
+			
 
-	// build next and previous links
-	// data._links.next: "https://api.twitch.tv/kraken/search/streams?limit=10&offset=10&q=starcraft"
-	// data._links.self: "https://api.twitch.tv/kraken/search/streams?limit=10&offset=0&q=starcraft"
+		} // end for loop
+		feedContainer.appendChild(feedDiv);	
+	} // end buildFeed
+	buildFeed();
     
 }
 
