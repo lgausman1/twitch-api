ea45b61a4b14436aae5f7213334d86af5e705aa6
lgausman1 <l.gausman@comcast.net>
2015-11-11 08:38:53 -0800

    cleaned up code a bit, moved prev and next buttons to html

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 // set up variables for search query and pagination
 var streamSrc = 'starcraft';
 var offset = 0;
-
+//default empty string value
 var emptyString = "";
-var searchBtn = document.getElementById('searchBtn');
-var searchInput = document.getElementById('streamSearch');
 
+var searchBtn = document.getElementById('searchBtn');
 searchBtn.addEventListener('click', searchStreams, false);
+
 function searchStreams() {
+	var searchInput = document.getElementById('streamSearch');
 	// if searchInput value is null 
 	if(searchInput.value.length === 0) {
 		alert("enter a stream to search");
 	}
-	// else streamSrc equals input value
 	else {
 		offset = 0; // reset offset for each search
-		streamCountText.textContent = (offset + 1) + "/" + (offset + 10); // reset streamCount
+		streamCountText.textContent = (offset + 1) + "/" + (offset + 10); 
 		streamSrc = searchInput.value;
 		// find and remove previous script query from DOM
 		var oldScript = document.getElementById('script');
 		document.head.removeChild(oldScript);
 		buildQuery();
-		searchInput.value = ""; // reset input value
+		searchInput.value = emptyString; // reset input value
 	}
 }
 
@@ -35,17 +35,9 @@ function buildQuery() {
 	head.appendChild(script);	
 }
 
-var prevStream = document.getElementById("prevStream");
-var prevText = document.createElement('i');
-prevText.classList.add("fa");
-prevText.classList.add("fa-chevron-circle-left");
-prevStream.appendChild(prevText);
-
-var nextStream = document.getElementById("nextStream");
-var nextText = document.createElement('i');
-nextText.classList.add("fa");
-nextText.classList.add("fa-chevron-circle-right");
-nextStream.appendChild(nextText);
+// pagination elements
+var prevStream = document.getElementById("prevStreamBtn");
+var nextStream = document.getElementById("nextStreamBtn");
 
 var streamCount = document.getElementById('streamCount');
 var streamCountText = document.createElement('p');
@@ -54,7 +46,7 @@ streamCount.appendChild(streamCountText);
 
 (function buildPagination() {
 
-	nextText.addEventListener('click', function() {
+	nextStream.addEventListener('click', function() {
 		offset = offset + 10;
 		streamCountText.textContent = (offset + 1) + "/" + (offset + 10);
 		console.log(offset);
@@ -64,8 +56,7 @@ streamCount.appendChild(streamCountText);
 		buildQuery();
 	}, false); // end nextLink click event
 
-	prevText.addEventListener('click', function() {
-		// if 
+	prevStream.addEventListener('click', function() {
                 if(offset <= 0) {
                    return false; 
                 }
@@ -84,7 +75,6 @@ streamCount.appendChild(streamCountText);
 
 // retrieve data from api
 function myCallback(data){
-
   	console.log(data);
 	// declare variables and build feeds
 	var feedContainer = document.getElementById("feeds");
@@ -92,7 +82,6 @@ function myCallback(data){
 	var feedTotalDiv = document.getElementById("feedTotal");
 	var feedTotalP = document.createElement('p');
 	feedTotalP.textContent = "Total results: " + data._total;
-
 	var feedList = document.createElement('ul');
 	var streamsLen = data.streams.length;
 
@@ -101,27 +90,27 @@ function myCallback(data){
 		feedContainer.innerHTML = emptyString;
 
 		for (var i = 0; i < streamsLen; i++) {
-                        // set up variables for feed list
+            // set up variables for feed list
 			var li = document.createElement('li'),
-                        streamTitle = document.createElement('h2'),
-                        streamLink = document.createElement('a'),
-                        img = new Image(),
-                        gameTitle = document.createElement('p'),
-                        viewerSpan = document.createElement('span'),
-                        gameDesc = document.createElement('p');
+                    streamTitle = document.createElement('h2'),
+                    streamLink = document.createElement('a'),
+                    img = new Image(),
+                    gameTitle = document.createElement('p'),
+                    viewerSpan = document.createElement('span'),
+                    gameDesc = document.createElement('p');
 
 			img.src = data.streams[i].preview.medium;
 			streamTitle.textContent = data.streams[i].channel.display_name;
-                        streamLink.setAttribute('href', data.streams[i].channel.url);
-                        streamLink.setAttribute('target', '_blank');
-                        streamLink.appendChild(streamTitle);
-                        gameTitle.classList.add('streamSubTitle');
+            streamLink.setAttribute('href', data.streams[i].channel.url);
+            streamLink.setAttribute('target', '_blank');
+            streamLink.appendChild(streamTitle);
+            gameTitle.classList.add('streamSubTitle');
 			gameTitle.textContent = data.streams[i].game;
 			viewerSpan.textContent = " - " + data.streams[i].viewers + " viewers";
 			viewerSpan.className = 'viewers';
 			gameDesc.textContent = data.streams[i].channel.status;
 			li.appendChild(img);
-                        li.appendChild(streamLink);
+            li.appendChild(streamLink);
 			li.appendChild(gameTitle);
 			gameTitle.appendChild(viewerSpan);
 			li.appendChild(gameDesc);
@@ -129,18 +118,14 @@ function myCallback(data){
 			feedDiv.appendChild(feedList);
 			
 		} // end for loop
-		feedTotalDiv.innerHTML = emptyString;
+		feedTotalDiv.innerHTML = emptyString; // clear previous total
 		feedTotalDiv.appendChild(feedTotalP);
-		
 		feedContainer.appendChild(feedDiv);	
-	
 	}());
 	
 	// if no search results render message to page
 	if(streamsLen < 1) {
 		var noResults = "Sorry, your search query returned 0 results. Please try again.";
                 feedContainer.innerHTML = noResults;
-	}
-    
+	} 
 } // end myCallback
-
